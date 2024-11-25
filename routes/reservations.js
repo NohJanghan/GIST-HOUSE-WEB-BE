07a44074aca4dfb91bc6cb78a863c98c8bf11166
Janghan Noh <felkared@gmail.com>
2024-11-26 01:09:42 +0900

    Feature: getPlacereservation 이름 변경

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const getPlacereservation = require("../controllers/reservation/placereservation");
+const getPlacereservation = require("../controllers/reservation/getPlacereservation");
 const postPlacereservation = require("../controllers/reservation/postPlacereservation");
 
 const router = express.Router();

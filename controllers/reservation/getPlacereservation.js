fd3085382ae89dbc7b31bebc8b7ba5cce50fa1d9
Minkyung Kim <kimminkyung@gm.gist.ac.kr>
2024-11-30 01:11:49 +0900

    getPlacereservation update

--- controllers/reservation/getPlacereservation.js
+++ controllers/reservation/getPlacereservation.js
@@ -1,13 +1,50 @@
+const prisma = require('../../prismaClient')
+
 // 예약 조회
 async function getPlacereservation(req, res) {
   try {
-    const reservations = await prisma.place_reservation.findMany({
-      include: { facility: true, student: true },
+    // 전체 예약 조회
+    if(!req.query || !req.query.roomId || !req.query.YYYYMMDD) {
+      return res.status(400).send('Bad Request')
+    }
+    let roomId = req.query.roomId
+    roomId = Number(roomId)
+    let date = req.query.YYYYMMDD
+    // 날짜 형식 변경
+    date = new Date(
+      date.toString().slice(0, 4),  // 연도
+      date.toString().slice(4, 6) - 1,  // 월 (0부터 시작)
+      date.toString().slice(6, 8)   // 일
+    );
+    
+    const result = await prisma.place_reservation.findMany({
+      where: { date: date, facilityId: roomId },
+      select: {
+        reservation_id: true,
+        hour: true,
+        student: {
+          select: {
+            id: true
+          }
+        },
+      },
     });
-    res.json(reservations);
-  } catch (error) {
+
+    // 로그인 여부 확인 및 응답 반환
+    if (!req.session.userInfo) {
+      res.json({
+        reservations: result,
+      });
+    } else {
+      res.json({
+        reservations: result,
+        myReservation: myReservation,
+      });
+    }
+  } catch (e) {
+    console.error(e)
     res.status(500).json({ error: 'Failed to fetch reservations' });
   }
 }
 
-module.exports = getPlacereservation
+module.exports = getPlacereservation;
--- controllers/reservation/postPlacereservation.js
+++ controllers/reservation/postPlacereservation.js
@@ -1,14 +1,19 @@
 // 예약 생성
 async function postPlacereservation(req, res) {
-    const { date, hour, facilityId, studentId } = req.body;
-    try {
-        const newReservation = await prisma.place_reservation.create({
-            data: { date, hour, facilityId, studentId },
-        });
-        res.json(newReservation);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to create reservation' });
+  const { date, hour, facilityId } = req.body;
+  try {
+    if (!req.session.userInfo) {
+      return res.status(403).send('403 forbidden')
+    } else {
+      const userId = req.session.userInfo;
+      const newReservation = await prisma.place_reservation.create({
+        data: { date, hour, facilityId, userId },
+      });
+      res.json(newReservation);
     }
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to create reservation' });
+  }
 }
 
 module.exports = postPlacereservation

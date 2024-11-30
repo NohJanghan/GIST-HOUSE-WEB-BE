08083e7f7af752fd6e54ee161e485609a4840c9b
Janghan Noh <felkared@gmail.com>
2024-11-30 19:17:27 +0900

    Feature: 예약 신청기능 구현

--- controllers/reservation/postPlacereservation.js
+++ controllers/reservation/postPlacereservation.js
@@ -1,19 +1,60 @@
 // 예약 생성
 async function postPlacereservation(req, res) {
-  const { date, hour, facilityId } = req.body;
-  try {
-    if (!req.session.userInfo) {
-      return res.status(403).send('403 forbidden')
-    } else {
-      const userId = req.session.userInfo;
-      const newReservation = await prisma.place_reservation.create({
-        data: { date, hour, facilityId, userId },
-      });
-      res.json(newReservation);
+    // 쿼리가 누락된 경우 400 Bad request
+    if(!req.query || !req.query.roomId || !req.query.YYYYMMDD || !req.query.hour) {
+        return res.status(400).send('Bad request')
     }
-  } catch (error) {
-      res.status(500).json({ error: 'Failed to create reservation' });
-  }
+
+    // 로그인 되지 않은 경우에는 403 Forbidden
+    // TODO: 이후에 401 Unauthorized 로 변경 필요
+    if(!req.session.userInfo || !req.session.userInfo.student_id) {
+        return res.status(403).send('Forbidden')
+    }
+
+    // TODO: 잔여 시간을 확인하여 예약할 수 있는지 확인 필요
+    // TODO: 예약하기 전에 이미 그 시간에 예약한 사람이 있는지 확인 필요
+
+    // 쿼리로 받아온 값의 형식 변경
+    const date = new Date(
+        req.query.YYYYMMDD.slice(0, 4),  // 연도
+        req.query.YYYYMMDD.slice(4, 6) - 1,  // 월 (0부터 시작)
+        req.query.YYYYMMDD.slice(6, 8)   // 일
+    )
+    const hour = Array.isArray(req.query.hour) ?
+        req.query.hour.map(x => Number(x)) : [Number(req.query.hour)]
+    const roomId = Number(req.query.roomId)
+
+    // 요청 값 검증
+    if(isNaN(hour) || isNaN(roomId)) {
+        return res.status(400).send('Bad request')
+    }
+    else if(hour < 0 || hour > 23) {
+        return res.status(400).send('Bad request')
+    }
+
+    // 세션에서 student_id 형식을 숫자로 변경
+    const student_id = Number(req.session.userInfo.student_id)
+    try {
+        // 삽입할 데이터 설정
+        // 외래 키의 경우 connect를 이용하여 설정하는게 더 좋아 보이지만 createMany에서는 중첩된 connect를 지원하지 않음.
+        let data = []
+        hour.forEach(h => data.push({
+            studentId: student_id,
+            date: date,
+            hour: h,
+            facilityId: roomId,
+        }))
+
+        // 데이터베이스 조작
+        await prisma.place_reservation.createMany({
+            data: data
+        })
+
+        return res.status(200).send('OK')
+    } catch (e) {
+        console.error(e)
+        return res.status(500).send('Internal Server Error')
+    }
 }
 
 module.exports = postPlacereservation

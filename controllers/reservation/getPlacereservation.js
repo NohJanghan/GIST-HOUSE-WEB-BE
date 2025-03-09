f2dd4a83cd467a57ce7206569b01924c39e94ca1
Janghan Noh <felkared@gmail.com>
2025-03-09 22:10:00 +0900

    Feature: 사용자가 로그인일 때를 구분하여 예약 정보 전달

--- controllers/reservation/getPlacereservation.js
+++ controllers/reservation/getPlacereservation.js
@@ -14,7 +14,7 @@ async function getPlacereservation(req, res) {
     // 날짜 형식 변경
     date = parseDate(date)
     
-    const result = await prisma.place_reservation.findMany({
+    let result = await prisma.place_reservation.findMany({
       where: {
         date: date,
         facilityId: roomId
@@ -30,35 +30,29 @@ async function getPlacereservation(req, res) {
       },
     });
 
-    let myReservation = null;
-    if (req.session.userInfo) {
-      const userId = req.session.userInfo.id;
-      myReservation = await prisma.place_reservation.findMany({
-        where: {
-          date: date,
-          facilityId: roomId,
-          student: {
-            id: userId
-          },
-        },
-        select: {
-          reservation_id: true,
-          hour: true,
-        },
-      });
+    console.log(req.session)
+    console.log(req.session.userInfo)
+    // 로그인이 된 경우에는 구분하여 출력 & 학번 가림
+    if (req.session.userInfo && req.session.userInfo.student_id) {
+      const studentId = Number(req.session.userInfo.student_id)
+
+      let myReservations = result.filter((reservation) => reservation.student.id === studentId)
+      let otherReservations = result.filter((reservation) => reservation.student.id !== studentId)
+
+      // 학번 정보 삭제
+      myReservations.forEach((reservation) => {delete reservation.student})
+      otherReservations.forEach((reservation) => {delete reservation.student})
+
+      result = {reservations: otherReservations, myReservations: myReservations}
+    }
+    // 로그인 되지 않은 경우에는 학번만 가림
+    else {
+      result.forEach((reservation) => {delete reservation.student})
+      result = {reservations: result, myReservations: []}
     }
 
-    // 로그인 여부 확인 및 응답 반환
-    if (!req.session.userInfo) {
-      res.json({
-        reservations: result,
-      });
-    } else {
-      res.json({
-        reservations: result,
-        myReservation: myReservation,
-      });
-    }
+    // 응답 반환
+    res.json(result)
   } catch (e) {
     console.error(e)
     res.status(500).json({ error: 'Failed to fetch reservations' });
